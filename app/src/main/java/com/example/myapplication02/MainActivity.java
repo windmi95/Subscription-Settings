a6037e89951607fbf8b4e12977d4ebf402e43028
windmi95 <windmi95@gmail.com>
2024-04-07 22:57:00 +0900

    join_edit0-2

--- app/src/main/java/com/example/myapplication02/MainActivity.java
+++ app/src/main/java/com/example/myapplication02/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.myapplication02;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    String TAG = this.getClass().getSimpleName();
+    EditText editText;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.nickname);
+        button = findViewById(R.id.next_btn);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Log.e(TAG, "beforeTextChanged() - charSequence : " + charSequence);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Log.e(TAG, "onTextChanged() - charSequence : " + charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Log.e(TAG, "afterTextChanged() - editable : " + editable.toString());
+            }
+        });
+
+
     }
 }
